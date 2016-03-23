06fabac53189318f4685f638855deef4db174205
slince <taosikai@yeah.net>
2016-03-23 16:13:11 +0800

    修改demo

--- demo/config/app.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-    'sites' => array(
-        '译言网-精选' => 'http://feed.yeeyan.org/select',
-       // '译言网-最新译文' => 'http://feed.yeeyan.org/latest',
-    ),
-);
--- demo/index.php
+++ demo/index.php
@@ -1,16 +1,16 @@
 <?php
-use Slince\Config\Config;
-use Slince\RssCollector\RssCollector;
 include __DIR__ . '/../vendor/autoload.php';
 
-$config = new Config();
-$config->load(__DIR__ . '/config/app.php');
+use Slince\RssCollector\RssCollector;
 
-foreach ($config->get('sites') as $rssUrl) {
-    $rssCollector = new RssCollector($rssUrl);
-    $rssCollector->pushHandler(function(&$title, &$content, &$link){
-        file_put_contents(iconv('utf-8', 'gbk', __DIR__ . "/data/{$title}.txt"), $content);
-    });
-    $data = $rssCollector->run()->getData();
-    var_dump($data);
-}
+//url源
+$rssUrl = 'http://feed.yeeyan.org/select';
+//创建采集客户端
+$rssCollector = new RssCollector($rssUrl);
+//添加文章处理器
+$rssCollector->pushHandler(function(&$title, &$content, &$link){
+    @file_put_contents(iconv('utf-8', 'gbk', __DIR__ . "/data/{$title}.txt"), $content);
+});
+//运行并获取执行结果
+$data = $rssCollector->run()->getData();
+var_dump($data);
